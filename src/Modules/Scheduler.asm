0c99ae5035a8c7ce9452b13b7748ab503c2f41b4
Chris L <stylesuxx@gmail.com>
2023-06-13 18:57:15 +0200

    Improved documentation for Scheduler  module

--- src/Modules/Scheduler.asm
+++ src/Modules/Scheduler.asm
@@ -22,17 +22,28 @@
 ; along with Bluejay.  If not, see <http://www.gnu.org/licenses/>.
 ;
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
+;
+; Scheduler
+;
+;**** **** **** **** **** **** **** **** **** **** **** **** ****
 
-; Divider of scheduler counter as a power of 2 number
-; This macro exists for convenience to easily change scheduler step time
-; In the table below you can find valid values for this parameter
+;**** **** **** **** **** **** **** **** **** **** **** **** ****
+;
+; Divider of scheduler counter as a power of 2 number.
+;
+; This macro exists for convenience to easily change scheduler step time.
+; In the table below you can find valid values for this parameter.
+;
 ; Value     Step    Cycle (8 steps)     Zero crossing scan interference
+; ---------------------------------------------------------------------
 ; 0         16ms    128ms               Medium - High
 ; 1         32ms    256ms               Medium
 ; 2         64ms    512ms               Low
 ; 3         128ms   1024ms              Low
 ; 4         256ms   2048ms              Low
 ; 5         512ms   4096ms              Low
+;
+;**** **** **** **** **** **** **** **** **** **** **** **** ****
 SCHEDULER_COUNTER_DIVIDER_POW2  EQU 3
 
 ; Bitmask to get the remainder of dividing a number by 8
@@ -46,26 +57,26 @@ STEP1_STATUS_FRAME              EQU (001h SHL SCHEDULER_COUNTER_DIVIDER_POW2)
 STEP3_DEBUG1_FRAME              EQU (003h SHL SCHEDULER_COUNTER_DIVIDER_POW2)
 STEP5_DEBUG2_FRAME              EQU (005h SHL SCHEDULER_COUNTER_DIVIDER_POW2)
 
-
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
 ;
 ; Scheduler
-;   Each step 128ms, cycle 1024ms (8 steps)
 ;
-;   ReqSch00:   - Steps even [0, 2, 4, 6]
-;   ReqSch01:       - Update temperature setpoint
-;   ReqSch02:       - [TELEMETRY] Send demag metric frame
-;   ReqSch03:   - Steps odd [1, 3, 5, 7]
-;   ReqSch04:       - Update temperature PWM limit
-;   ReqSch06:       - Case step 1
-;   ReqSch07:           - [TELEMETRY] Send status frame
-;   ReqSch08:       - Case step 3
-;   ReqSch09:           - [TELEMETRY] Send debug1 frame
-;   ReqSch10:       - Case step 5
-;   ReqSch11:           - [TELEMETRY] Send debug2 frame
-;   ReqSch12:       - Case step 7
-;   ReqSch13:           - [TELEMETRY] Send temperature frame
-;   ReqSch14:       - Start new ADC conversion
+; Each step is 128ms, a complete cycle is 1024ms (8 steps)
+;
+; ReqSch00:   - Steps even [0, 2, 4, 6]
+; ReqSch01:       - Update temperature setpoint on every even step
+; ReqSch02:       - [TELEMETRY] Send demag metric frame
+; ReqSch03:   - Steps odd [1, 3, 5, 7]
+; ReqSch04:       - Update temperature PWM limit on every odd step
+; ReqSch06:       - Case step 1
+; ReqSch07:           - [TELEMETRY] Send status frame
+; ReqSch08:       - Case step 3
+; ReqSch09:           - [TELEMETRY] Send debug1 frame
+; ReqSch10:       - Case step 5
+; ReqSch11:           - [TELEMETRY] Send debug2 frame
+; ReqSch12:       - Case step 7
+; ReqSch13:           - [TELEMETRY] Send temperature frame
+; ReqSch14:       - Start new ADC conversion after every odd step
 ;
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
 
@@ -97,7 +108,7 @@ scheduler_steps_even:
 ; UPDATE TEMPERATURE SETPOINT
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
 
-    ; Check temp protection enabled, and skip when protection is disabled
+    ; Check temp protection: skip when protection is disabled
     mov  A, Temp_Prot_Limit
     jz   scheduler_steps_even_demag_metric_frame
 
@@ -106,11 +117,13 @@ scheduler_steps_even:
 
     ; Check TEMP_LIMIT in Base.inc and make calculations to understand temperature readings
     ; Is temperature reading below 256?
-    ; On BB1 & BB21
-    ;    - Using external voltage regulator and vdd 3.3V as ADC reference -> ADC 10bit value corresponding to about 25ºC
-    ;    - Using external voltage regulator and internal 1.65V as ADC reference -> ADC 10bit value corresponding to about 0ºC
-    ; On BB51
-    ;    - Using external voltage regulator and internal 1.65V as ADC reference -> ADC 10bit value corresponding to about 0ºC
+    ;
+    ; On BB1 & BB21:
+    ; - Using external voltage regulator and vdd 3.3V as ADC reference -> ADC 10bit value corresponding to about 25ºC
+    ; - Using external voltage regulator and internal 1.65V as ADC reference -> ADC 10bit value corresponding to about 0ºC
+    ;
+    ; On BB51:
+    ; - Using external voltage regulator and internal 1.65V as ADC reference -> ADC 10bit value corresponding to about 0ºC
     mov  A, ADC0H                       ; Load temp hi
     jz   scheduler_steps_even_demag_metric_frame ; Temperature below 25ºC (on 2S+ (BB1,BB2)) and below 0ºC (on 1S (BB1,BB21),BB51) do not update setpoint
 
@@ -139,17 +152,18 @@ scheduler_steps_even:
     ; Zero pwm cannot be set because of set_pwm_limit algo restrictions
     ; Otherwise hard stuttering is produced
 
+
 scheduler_steps_even_demag_metric_frame:
-    ; Check telemetry is enable to produce telemetry frames
+    ; Check if extended telemetry is enabled, otherwise we are done here.
     jb   Flag_Ext_Tele, scheduler_steps_even_demag_metric_frame_prepare
 
     ; No more work to do
     ret
 
-scheduler_steps_even_demag_metric_frame_prepare:
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
 ; [TELEMETRY] SEND DEMAG METRIC FRAME
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
+scheduler_steps_even_demag_metric_frame_prepare:
     mov  Ext_Telemetry_L, Demag_Detected_Metric ; Set telemetry low value to demag metric data
     mov  Ext_Telemetry_H, #0Ch          ; Set telemetry high value to demag metric frame ID
 
@@ -161,12 +175,12 @@ scheduler_steps_odd:
 ; UPDATE TEMPERATURE PWM LIMIT EVERY 256ms (ON ODD STEP)
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
 
-    ; Check temp protection enabled, and exit when protection is disabled
+    ; Check if temp protection is enabled, otherwise continue with odd step
     mov  A, Temp_Prot_Limit
     jz   scheduler_steps_odd_choose_step
 
-    ; pwm limit is updated one unit at a time to avoid abrupt pwm changes
-    ; resulting in current spikes, that may damage motor/esc
+    ; PWM limit is updated one unit at a time to avoid abrupt PWM changes
+    ; resulting in current spikes, that may damage motor/ESC.
     ; Compare pwm limit to setpoint
     clr  C
     mov  A, Pwm_Limit
@@ -178,26 +192,26 @@ scheduler_steps_odd_temp_pwm_limit_dec:
     ; Decrease pwm limit
     dec  Pwm_Limit
 
-    ; Now run speciffic odd scheduler step
+    ; Continue with odd scheduler step selection
     sjmp scheduler_steps_odd_choose_step
 
 scheduler_steps_odd_temp_pwm_limit_inc:
     ; Increase pwm limit
     inc  Pwm_Limit
 
-; Now run speciffic odd scheduler step
+; Run speciffic odd scheduler step
 scheduler_steps_odd_choose_step:
-    ; Check telemetry is enabled to produce telemetry frames
+    ; Check if extended telemetry is enabled, otherwise skip frame generation
     jnb  Flag_Ext_Tele, scheduler_steps_odd_restart_ADC
 
     ; Get scheduler step
     mov  A, Scheduler_Counter
     anl  A, #MASK_STEP
 
-scheduler_steps_odd_status_frame:
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
 ; [TELEMETRY] SEND STATUS FRAME
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
+scheduler_steps_odd_status_frame:
     cjne A, #STEP1_STATUS_FRAME, scheduler_steps_odd_debug1_frame
 
     ; if (Demag_Detected_Metric_Max >= 120)
@@ -239,10 +253,10 @@ scheduler_steps_odd_status_frame_max_loaded:
     ; Nothing else to do
     ret
 
-scheduler_steps_odd_debug1_frame:
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
 ; [TELEMETRY] SEND DEBUG1 FRAME
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
+scheduler_steps_odd_debug1_frame:
     cjne A, #STEP3_DEBUG1_FRAME, scheduler_steps_odd_debug2_frame
 
     ; Stub for debug 1
@@ -255,10 +269,10 @@ scheduler_steps_odd_debug1_frame:
     ; Nothing else to do
     ret
 
-scheduler_steps_odd_debug2_frame:
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
 ; [TELEMETRY] SEND DEBUG2 FRAME
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
+scheduler_steps_odd_debug2_frame:
     cjne A, #STEP5_DEBUG2_FRAME, scheduler_steps_odd_temperature_frame
 
     ; Stub for debug 2
@@ -271,10 +285,10 @@ scheduler_steps_odd_debug2_frame:
     ; Nothing else to do
     ret
 
-scheduler_steps_odd_temperature_frame:
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
 ; [TELEMETRY] SEND TEMPERATURE FRAME
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
+scheduler_steps_odd_temperature_frame:
     ; If step is not 1, 3 or 5 it has to be 7, so cjmp is not necessary
     ; cjne A, #STEP7_TEMPERATURE_FRAME, scheduler_steps_odd_restart_ADC
 
@@ -290,10 +304,10 @@ IF MCU_TYPE == MCU_BB1 or MCU_TYPE == MCU_BB2
     cjne @Temp1, #01h, scheduler_steps_odd_temperature_frame_power_rating_2s
 ENDIF
 
-scheduler_steps_odd_temperature_frame_power_rating_1s:
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
 ; ON BB51 and BB1, BB2 at 1S, all using internal 1.65V ADC reference
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
+scheduler_steps_odd_temperature_frame_power_rating_1s:
     mov  A, ADC0H
     jnz  scheduler_steps_odd_temperature_frame_pr1s_temperature_above_0
 
@@ -345,12 +359,11 @@ scheduler_steps_odd_temperature_frame_temp_load:
     mov  Ext_Telemetry_L, A             ; Set telemetry low value with temperature data
     mov  Ext_Telemetry_H, #02h          ; Set telemetry high value on first repeated dshot coding partition
 
-; Now restart ADC conversion
-scheduler_steps_odd_restart_ADC:
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
 ; START NEW ADC CONVERSION
 ;**** **** **** **** **** **** **** **** **** **** **** **** ****
-    ; Now restart ADC conversion
+scheduler_steps_odd_restart_ADC:
+    ; Restart ADC conversion
     Restart_Adc
 
     ; Nothing else to do
