6774acc7a51991b022e315dfce395bd3b43f9e4a
HAKRC <33365327+HAKRC@users.noreply.github.com>
2024-07-22 13:57:23 +0800

    Update Bluejay.asm

--- src/Bluejay.asm
+++ src/Bluejay.asm
@@ -170,7 +170,7 @@ DEFAULT_PGM_LED_CONTROL EQU 0           ; Byte for LED control. 2 bits per LED,0
 DEFAULT_PGM_STARTUP_POWER_MIN EQU 21    ; 0..255 => (1000..1125 Throttle): value * (1000 / 2047) + 1000
 DEFAULT_PGM_STARTUP_BEEP EQU 1          ; 0=Short beep,1=Melody
 
-DEFAULT_PGM_STARTUP_POWER_MAX EQU 50     ; 0..255 => (1000..2000 Throttle): Maximum startup power
+DEFAULT_PGM_STARTUP_POWER_MAX EQU 5     ; 0..255 => (1000..2000 Throttle): Maximum startup power
 DEFAULT_PGM_BRAKING_STRENGTH EQU 255    ; 0..255 => 0..100 % Braking
 
 DEFAULT_PGM_SAFETY_ARM EQU 0            ; EDT safety arm is disabled by default
